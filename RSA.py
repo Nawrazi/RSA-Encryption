f248235286326b377483f55d365d3e3b53d90d4e
Nazrawi Demeke <dnazrawi@gmail.com>
2021-09-29 04:28:20 +0300

    Final Version

--- RSA.py
+++ RSA.py
@@ -6,9 +6,10 @@ def isPrime(x):
             return False
     return True
 
-def primeGenerator():
+# Generates an n-digit prime number
+def primeGenerator(n):
     string = ""
-    for i in range(7):
+    for i in range(n):
         x=random.randint(1,9)
         string += str(x)
     number = int(string)
@@ -21,18 +22,77 @@ def primeGenerator():
             return number
         number+=2
 
+# Finds the GCD of x and y using Euclid's Algorithm
 def gcd(x,y):
     if y==0:
         return x
     else:
         return gcd(y,x%y)
 
-p = primeGenerator()
-q = primeGenerator()
-e = primeGenerator()**2
-n = p*q
-r = (p-1)*(q-1)     # Euler's Totient
-publicKey = (e,n)
+# Finds the multiplicative inverse of a mod b
+def pulverizer(a,b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd, x, y = pulverizer(b % a, a)
+        d = y - (b // a) * x
+        return gcd, d, x
 
-def modularInverse(a,b):
-    pass
+# Generates a secret key and a public key
+def keyGen():
+    numberOfDigits = 3
+    p = primeGenerator(numberOfDigits)        # First random prime number
+    q = primeGenerator(numberOfDigits)        # Second random prime number
+
+    n = p*q
+    r = (p-1)*(q-1)              # Euler's Totient
+    e = primeGenerator(numberOfDigits)**2     # A number that is highly likely to be relatively prime with r
+
+    publicKey = (e,n)
+
+    g, d, y = pulverizer(e,r)
+    if d<0:
+        d %= r
+
+    secretKey = (d,n)
+
+    return secretKey, publicKey
+
+def encrypt(message, publicKey):
+    e,n = publicKey
+    encryptedMessage = []
+    for letter in message:
+        letter = ord(letter)
+        if gcd(letter,n) != 1:
+            raise Exception('UNEXPECTED ERROR, TRY AGAIN')  # For security, but highly unlikely to happen
+
+        encryptedLetter = (letter**e)%n
+        encryptedMessage.append(encryptedLetter)
+
+    return encryptedMessage
+
+def decrypt(encryptedMessage, secretKey):
+    d,n = secretKey
+    originalMessage = ""
+
+    for number in encryptedMessage:
+        number = int(number)
+        originalNumber = (number**d)%n
+
+        originalMessage += chr(originalNumber)
+
+    return originalMessage
+
+def main():
+    secretKey, publicKey = keyGen()
+
+    message = input("Input the message to encrypt: ")
+    encryptedMessage = encrypt(message,publicKey)
+    print('*'*100)
+    print("Encrypted text:", encryptedMessage)
+    print("The secret key is:", secretKey)
+
+    originalMessage = decrypt(encryptedMessage,secretKey)
+    print("Original Text:", originalMessage)
+
+main()
